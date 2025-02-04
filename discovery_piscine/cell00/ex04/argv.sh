c9c6c94d14df37beb49b23aae870ec63a4340ad8
108Thanawat <64050108@kmitl.ac.th>
2025-02-05 01:56:45 +0700

    ex04

---
+++ discovery_piscine/cell00/ex04/argv.sh
@@ -0,0 +1,11 @@
+if [ "$#" -eq 0 ]; then
+    echo "No arguments supplied"
+elif [ "$#" -le 3 ]; then
+    for arg in "$@"; do
+        echo "$arg"
+    done
+else
+    for i in {1..3}; do
+        echo "${!i}"
+    done
+fi
